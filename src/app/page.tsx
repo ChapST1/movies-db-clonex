26d123e2f912710e1b2851650a84a0c685ef1ef5
ChapST1 <cutitox59@gmail.com>
2024-02-07 22:24:27 -0500

    add: Home page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
-import {Button} from "@/components/ui/button";
+import type {DbResponse, MovieTrend} from "@/types";
 
-export default function HomePage() {
-  return <Button>👋</Button>;
+import {Title} from "@/components/ui/title";
+import {getTrendingMovies} from "@/lib/services/get-trending-movies";
+import {MoviesGrid} from "@/components/movie/movies-grid";
+
+export default async function HomePage() {
+  const movies = (await getTrendingMovies({time: "day"})) as DbResponse<MovieTrend>;
+
+  return (
+    <section>
+      <Title>Trending Movies</Title>
+      <MoviesGrid movies={movies.results} />
+    </section>
+  );
 }
