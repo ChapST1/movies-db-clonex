b31fd57d5999a77f84ef75f19a1592c2cd0930ff
ChapST1 <cutitox59@gmail.com>
2024-02-13 01:55:21 -0500

    feat: Movie hero component

--- src/components/movie-details/movie-hero/movie-hero-info.tsx
+++ src/components/movie-details/movie-hero/movie-hero-info.tsx
@@ -2,7 +2,7 @@ import {twMerge} from "tailwind-merge";
 
 export function MovieHeroInfo({className, children, ...props}: React.ComponentProps<"div">) {
   return (
-    <div className={twMerge("pt-10", className)} {...props}>
+    <div className={twMerge("", className)} {...props}>
       {children}
     </div>
   );
