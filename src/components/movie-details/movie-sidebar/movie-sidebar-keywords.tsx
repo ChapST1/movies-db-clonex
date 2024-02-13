00cd5f173d8658f2fc72361a3488a72bb69a760e
ChapST1 <cutitox59@gmail.com>
2024-02-13 01:50:24 -0500

    feat: add movie keywords Component

---
+++ src/components/movie-details/movie-sidebar/movie-sidebar-keywords.tsx
@@ -0,0 +1,19 @@
+import type {Movie, MovieKeywords} from "@/types";
+
+import {Badge} from "@/components/ui/badge";
+import {getMovieKeywords} from "@/lib/services/get-movie-keywords";
+
+export async function MovieSidebarKeywords({id}: {id: Movie["id"]}) {
+  const {keywords} = (await getMovieKeywords(id)) as MovieKeywords;
+
+  return (
+    <div>
+      <h2 className="font-bold">Keywords</h2>
+      <aside className="flex flex-wrap justify-start gap-2 py-2">
+        {keywords.map(({id, name}) => (
+          <Badge key={id}>{name}</Badge>
+        ))}
+      </aside>
+    </div>
+  );
+}
