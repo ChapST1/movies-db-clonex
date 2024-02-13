2603630e443b80594dcd039f96b07cecf1538432
ChapST1 <cutitox59@gmail.com>
2024-02-13 02:01:53 -0500

    add: upgrade tooltip component

--- src/components/tooltip.tsx
+++ src/components/tooltip.tsx
@@ -1,19 +1,26 @@
 import clsx from "clsx";
+import {twMerge} from "tailwind-merge";
 
-interface TooltipProps {
+interface TooltipProps extends React.ComponentProps<"div"> {
   children: React.ReactNode;
   tooltipContent: string;
   tooltipPosition: "top" | "bottom";
 }
 
-export function Tooltip({children, tooltipContent, tooltipPosition = "bottom"}: TooltipProps) {
+export function Tooltip({
+  children,
+  tooltipContent,
+  tooltipPosition = "bottom",
+  className,
+  ...props
+}: TooltipProps) {
   const position = clsx({
     "top-[-20px] group-hover/tooltip:top-[-30px] ": tooltipPosition === "top",
     "bottom-[-20px] group-hover/tooltip:bottom-[-30px]": tooltipPosition === "bottom",
   });
 
   return (
-    <div className="group/tooltip relative">
+    <div className={twMerge("group/tooltip relative", className)} {...props}>
       {children}
       <span
         className={`invisible  absolute left-[50%] z-30  w-[max-content] translate-x-[-50%] rounded-md border border-accent bg-background px-2 py-1  text-xs opacity-0 delay-200 duration-300 group-hover/tooltip:visible  group-hover/tooltip:opacity-100 ${position}`}
