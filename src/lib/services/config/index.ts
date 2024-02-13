15069c7f452b07b07eb3e8118aad29d7b5eda41a
ChapST1 <cutitox59@gmail.com>
2024-02-13 01:41:34 -0500

    add: Service api urls

--- src/lib/services/config/index.ts
+++ src/lib/services/config/index.ts
@@ -17,3 +17,23 @@ export const GENERATE_TRENDING_MOVIES_API_URL = ({
 }: GenerateTrendingMoviesApiUrlProps) => {
   return `https://api.themoviedb.org/3/trending/movie/${time}?api_key=${API_KEY}&page=${page}`;
 };
+
+export const generateMovieByIdApiUrl = (id: number) => {
+  return `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`;
+};
+
+export const generateMoviesCastApiUrl = (id: number) => {
+  return `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${API_KEY}`;
+};
+
+export const generateMoviesImagesApiUrl = (id: number) => {
+  return `https://api.themoviedb.org/3/movie/${id}/images?api_key=${API_KEY}`;
+};
+
+export const generateMoviesKeywordsApiUrl = (id: number) => {
+  return `https://api.themoviedb.org/3/movie/${id}/keywords?api_key=${API_KEY}`;
+};
+
+export const generateMovieRecomendationsApiUrl = (id: number) => {
+  return `https://api.themoviedb.org/3/movie/${id}/recommendations?api_key=${API_KEY}`;
+};
