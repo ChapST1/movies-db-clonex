6303619894f8fa73fd0d61f2ffa6c51aa18cd502
ChapST1 <cutitox59@gmail.com>
2024-02-14 01:29:28 -0500

    style: upgrade Title component

--- src/components/ui/title.tsx
+++ src/components/ui/title.tsx
@@ -1,5 +1,14 @@
 import {twMerge} from "tailwind-merge";
 
 export function Title({className, ...props}: React.ComponentProps<"h2">) {
-  return <h2 className={twMerge("py-3 text-xl", className)}>{props.children}</h2>;
+  return (
+    <h2
+      className={twMerge(
+        "sticky top-[120px] z-20  bg-gradient-to-b from-background via-background/50 via-80% py-5 text-xl md:top-16",
+        className,
+      )}
+    >
+      {props.children}
+    </h2>
+  );
 }
