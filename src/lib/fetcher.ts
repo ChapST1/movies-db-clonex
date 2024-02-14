588811bb875200cd4b3ae345b73763bf7ef8f80e
ChapST1 <cutitox59@gmail.com>
2024-02-14 01:18:03 -0500

    type: Utility

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -1,6 +1,6 @@
-export async function fetcher<T>(...args) {
+export async function fetcher<T>(input: string | URL | globalThis.Request, init?: RequestInit) {
   try {
-    const res = await fetch(...args);
+    const res = await fetch(input, init);
     const data = (await res.json()) as T;
 
     return data;
