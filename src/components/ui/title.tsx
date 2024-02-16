ceee79424ce622bac7f1d9c7468f523aabb725e2
ChapST1 <cutitox59@gmail.com>
2024-02-16 00:56:22 -0500

    style: upgrade component

--- src/components/ui/title.tsx
+++ src/components/ui/title.tsx
@@ -4,7 +4,8 @@ export function Title({className, ...props}: React.ComponentProps<"h2">) {
   return (
     <h2
       className={twMerge(
-        "sticky top-[120px] z-20  bg-gradient-to-b from-background via-background/50 via-80% py-5 text-xl md:top-16",
+        // "sticky top-[120px] z-20  bg-gradient-to-b from-background via-background/50 via-80% py-5 text-xl md:top-16",
+        "sticky top-[120px] z-20 bg-background/85 py-3 text-xl  backdrop-blur-sm  md:top-16",
         className,
       )}
     >
