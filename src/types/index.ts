6754c24fdecf00a027af942a4c856c951c65b690
ChapST1 <cutitox59@gmail.com>
2024-02-16 00:55:23 -0500

    types: edit file

--- src/types/index.ts
+++ src/types/index.ts
@@ -170,26 +170,15 @@ export interface Recomendations {
   backdrop_path: string;
   id: number;
   title: string;
-  original_language: OriginalLanguage;
+  original_language: string;
   original_title: string;
   overview: string;
   poster_path: string;
-  media_type: MediaType;
+  media_type: string;
   genre_ids: number[];
   popularity: number;
   release_date: string;
   video: boolean;
   vote_average: number;
   vote_count: number;
-}
-
-export enum MediaType {
-  Movie = "movie",
-}
-
-export enum OriginalLanguage {
-  En = "en",
-  Es = "es",
-  Fr = "fr",
-  Ko = "ko",
 }
