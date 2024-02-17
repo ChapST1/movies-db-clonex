eecf5a6eb7de5bf92206759ad991366261f137fd
ChapST1 <cutitox59@gmail.com>
2024-02-17 02:37:50 -0500

    style: use a primary color

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,7 +7,9 @@ export function Navbar() {
   return (
     <header className="sticky left-0 top-0 z-50 flex h-[120px] w-full flex-col items-center justify-between gap-2 border-b border-border bg-background/85 px-3 py-3 backdrop-blur-sm md:h-16 md:flex-row">
       <Link href="/">
-        <h2 className="text-2xl font-semibold">Movies DBX</h2>
+        <h2 className="text-gradient text-2xl font-semibold md:text-3xl md:font-bold">
+          Movies DBX
+        </h2>
       </Link>
 
       <div className="flex items-center gap-3">
