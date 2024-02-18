86d20182ee1617c9d3417d03b8214c711d8db16f
ChapST1 <cutitox59@gmail.com>
2024-02-18 03:10:21 -0500

    fix:  href attribute

--- src/components/movie-details/movie-characters/index.tsx
+++ src/components/movie-details/movie-characters/index.tsx
@@ -25,7 +25,7 @@ export async function MovieCharacters({id}: {id: Movie["id"]}) {
 
             return (
               <article key={cast_id} className="relative">
-                <Link href={`/people/${id}`}>
+                <Link href={`/movies/people/${id}`}>
                   <picture className="block h-full overflow-hidden rounded-md">
                     <img
                       alt={image}
