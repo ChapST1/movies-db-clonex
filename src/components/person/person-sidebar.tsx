039b10a22c9a33411692964c246ccf6c60e7a528
ChapST1 <cutitox59@gmail.com>
2024-02-19 00:49:04 -0500

    style: Upgrade component

--- src/components/person/person-sidebar.tsx
+++ src/components/person/person-sidebar.tsx
@@ -13,7 +13,7 @@ export default function PersonSidebar({person}: {person: Person}) {
 
   return (
     <nav className=" sticky top-[120px] h-[calc(100dvh-120px)] overflow-hidden overflow-y-auto border-r border-border md:top-16 md:h-[calc(100dvh-64px)]">
-      <picture className="m-auto my-4 block w-[max-content] rounded-e-md bg-accent p-2">
+      <picture className="m-auto my-4 block w-[max-content] rounded-md bg-accent p-2">
         <img alt={name} className="rounded-sm" src={profilePath} />
       </picture>
 
