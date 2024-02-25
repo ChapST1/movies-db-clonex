a466edc062b1b40f9014b73347c9d4c73a8396b4
ChapST1 <cutitox59@gmail.com>
2024-02-25 01:29:17 -0500

    refactor: Search page

--- src/app/(routes)/search/page.tsx
+++ src/app/(routes)/search/page.tsx
@@ -1,49 +1,17 @@
-import type {DbResponse, Movie, Person, Tv} from "@/types";
+import {Suspense} from "react";
 
-import clsx from "clsx";
-
-import {Title} from "@/components/ui/title";
-import {MovieItem} from "@/components/movie/movie-item";
-import {PersonItem} from "@/components/person/person-item";
-import {TvItem} from "@/components/tv/tv-item";
-import {searchMultiMedia} from "@/lib/services/search-multi-media";
+import {SearchResults} from "@/components/search/search-results";
+import {MovieGridSkeleton} from "@/components/skeletons/movie-grid-skeleton";
 
 async function SearchPage({
-  searchParams: {q, movie, tv, people},
+  searchParams: {q, movie, tv, people, page = "1"},
 }: {
-  searchParams: {q: string; tv: string; movie: string; people: string};
+  searchParams: {q: string; tv: string; movie: string; people: string; page: string};
 }) {
-  const {results} = (await searchMultiMedia({query: q})) as DbResponse<Movie | Tv | Person>;
-
-  const containerStyles = clsx({
-    "*:block": !tv && !movie && !people,
-  });
-
   return (
-    <section>
-      <Title className="static text-center">Results for {q}</Title>
-      <div
-        className={`grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-3 ${containerStyles}`}
-      >
-        {results.map((item) => {
-          const {media_type} = item;
-
-          return (
-            <>
-              {media_type === "movie" && (
-                <MovieItem isFilter={{enable: Boolean(movie)}} movie={item as Movie} />
-              )}
-
-              {media_type === "tv" && <TvItem isFilter={{enable: Boolean(tv)}} tv={item as Tv} />}
-
-              {media_type === "person" && (
-                <PersonItem isFilter={{enable: Boolean(people)}} person={item as Person} />
-              )}
-            </>
-          );
-        })}
-      </div>
-    </section>
+    <Suspense key={`${q}${page}`} fallback={<MovieGridSkeleton />}>
+      <SearchResults movie={movie} page={page} people={people} q={q} tv={tv} />
+    </Suspense>
   );
 }
 
---
+++ src/components/search/search-results.tsx
@@ -0,0 +1,68 @@
+import type {DbResponse, Movie, Person, Tv} from "@/types";
+
+import clsx from "clsx";
+
+import {searchMultiMedia} from "@/lib/services/search-multi-media";
+
+import {Title} from "../ui/title";
+import {MovieItem} from "../movie/movie-item";
+import {TvItem} from "../tv/tv-item";
+import {PersonItem} from "../person/person-item";
+import {Badge} from "../ui/badge";
+
+import {SearchPagination} from "./search-pagination";
+
+export async function SearchResults({
+  page,
+  q,
+  tv,
+  movie,
+  people,
+}: {
+  page: string;
+  q: string;
+  tv: string;
+  movie: string;
+  people: string;
+}) {
+  const {results, total_pages, total_results} = (await searchMultiMedia({
+    query: q,
+    page,
+  })) as DbResponse<Movie | Tv | Person>;
+
+  const containerStyles = clsx({
+    "*:block": !tv && !movie && !people,
+  });
+
+  return (
+    <section>
+      <Title className="static flex items-center justify-center gap-2">
+        <Badge>{total_results}</Badge>
+        Results for {q}
+      </Title>
+      <div
+        className={`grid grid-cols-2 gap-3 md:grid-cols-[repeat(auto-fit,minmax(200px,1fr))] ${containerStyles}`}
+      >
+        {results.map((item) => {
+          const {media_type} = item;
+
+          return (
+            <>
+              {media_type === "movie" && (
+                <MovieItem isFilter={{enable: Boolean(movie)}} movie={item as Movie} />
+              )}
+
+              {media_type === "tv" && <TvItem isFilter={{enable: Boolean(tv)}} tv={item as Tv} />}
+
+              {media_type === "person" && (
+                <PersonItem isFilter={{enable: Boolean(people)}} person={item as Person} />
+              )}
+            </>
+          );
+        })}
+      </div>
+
+      <SearchPagination current={Number(page)} totalPages={total_pages} />
+    </section>
+  );
+}
