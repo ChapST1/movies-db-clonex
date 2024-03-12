d56dda176a8029b06701919dbee39a7ca455cef8
ChapST1 <cutitox59@gmail.com>
2024-03-12 02:57:39 -0400

    feat: show active link

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -6,34 +6,13 @@ import {siteConfig} from "@/lib/constants/site-config";
 import {Separator} from "../ui/separator";
 
 import {SidebarSearchMovie} from "./sidebar-search";
+import {SidebarLinks} from "./sidebar-links";
 
 export function Sidebar() {
   return (
-    <nav className="fixed  left-0 top-16 z-30 flex h-[calc(100dvh-64px)] w-[200px] flex-col gap-6 bg-background px-2 py-2">
+    <nav className="fixed  left-0 top-16 z-30 flex h-[calc(100dvh-64px)] w-[200px] flex-col gap-6 overflow-y-scroll bg-background px-2 py-2">
       <SidebarSearchMovie />
-      <ul className="flex flex-col gap-2">
-        {siteConfig.links.map(({name, path, icon}, index) => {
-          return (
-            <li key={index}>
-              <Link
-                className="flex items-center justify-start gap-2 rounded-md border-2 border-border bg-background p-2 duration-300 hover:border-gradient hover:bg-secondary hover:pl-3 hover:text-gradient [&>*:nth-child(1)]:size-4"
-                href={path}
-              >
-                {icon}
-                {name}
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
-      <Separator />
-      <Link
-        className="flex items-center justify-start gap-2 rounded-md border-2 border-border bg-background p-2 duration-300 hover:border-gradient hover:bg-secondary hover:pl-3 hover:text-gradient [&>*:nth-child(1)]:size-4"
-        href="path"
-      >
-        <FolderIcon />
-        Categories
-      </Link>
+      <SidebarLinks />
     </nav>
   );
 }
---
+++ src/components/sidebar/sidebar-links/index.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import {FolderIcon} from "lucide-react";
+import clsx from "clsx";
+import {usePathname} from "next/navigation";
+import Link from "next/link";
+
+import {siteConfig} from "@/lib/constants/site-config";
+import {Separator} from "@/components/ui/separator";
+
+export function SidebarLinks() {
+  const pathname = usePathname();
+
+  return (
+    <>
+      <ul className="flex flex-col gap-2">
+        {siteConfig.links.map(({name, path, icon}) => {
+          const activeStyles = clsx({
+            "border-gradient bg-secondary pl-3 text-gradien": path === pathname,
+            "border-border bg-background": path !== pathname,
+          });
+
+          return (
+            <li key={name}>
+              <Link
+                className={`flex w-full items-center justify-start gap-2 rounded-md border-2  p-2 duration-300 hover:border-gradient hover:bg-secondary hover:pl-3 hover:text-gradient [&>*:nth-child(1)]:size-4 ${activeStyles}`}
+                href={path}
+              >
+                {name}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+      <Separator />
+      <Link
+        className="flex items-center justify-start gap-2 rounded-md border-2 border-border bg-background p-2 duration-300 hover:border-gradient hover:bg-secondary hover:pl-3 hover:text-gradient [&>*:nth-child(1)]:size-4"
+        href="path"
+      >
+        <FolderIcon />
+        Categories
+      </Link>{" "}
+    </>
+  );
+}
