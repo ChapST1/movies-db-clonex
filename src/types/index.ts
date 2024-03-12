c19b5ad3f713bc76045ad84e96348c92be91ece7
ChapST1 <cutitox59@gmail.com>
2024-03-12 02:26:55 -0400

    type: Add types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,11 @@
 /**
+ * https://github.com/microsoft/TypeScript/issues/25760#issuecomment-405931434
+ */
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+
+export type WithOptional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
+
+/**
  * Movies Response
  */
 
