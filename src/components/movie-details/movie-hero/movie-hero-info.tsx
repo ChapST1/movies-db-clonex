aefe4ed6607d4e91ebc7c2135d2722dce47009ab
ChapST1 <cutitox59@gmail.com>
2024-03-15 19:24:14 -0400

    refactor: MovieHero - Component

--- src/components/movie-details/movie-hero/movie-hero-info.tsx
+++
@@ -1,9 +0,0 @@
-import {twMerge} from "tailwind-merge";
-
-export function MovieHeroInfo({className, children, ...props}: React.ComponentProps<"div">) {
-  return (
-    <div className={twMerge("", className)} {...props}>
-      {children}
-    </div>
-  );
-}
