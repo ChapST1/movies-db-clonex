c4802f9a721b4f9ce0b23f53c6d9cd4d528d5f85
ChapST1 <cutitox59@gmail.com>
2024-03-15 19:29:15 -0400

    refactor: Movie detail page

--- src/app/(routes)/movies/[id]/page.tsx
+++ src/app/(routes)/movies/[id]/page.tsx
@@ -1,38 +1,14 @@
-import type {MovieDetail} from "@/types";
+import type {MovieDetail as MovieDetailType} from "@/types";
 
-import {Suspense} from "react";
-
+import {MovieDetail} from "@/components/movie-details";
 import {getMovieById} from "@/lib/services/get-movie-by-id";
-import {MovieDetailHero} from "@/components/movie-details/movie-hero";
-import {MovieCharacters} from "@/components/movie-details/movie-characters";
-import {MovieImages} from "@/components/movie-details/movie-images";
-import {MovieRecomendations} from "@/components/movie-details/movie-recomendations";
-import {MoviesMediaSkeleton} from "@/components/skeletons/movies-media-skeleton";
-import {MovieDetailSidebar} from "@/components/movie-details/movie-sidebar";
-import {MovieGridSkeleton} from "@/components/skeletons/movie-grid-skeleton";
 
-export default async function MovieDetail({params: {id}}: {params: {id: MovieDetail["id"]}}) {
-  const movie = (await getMovieById(id)) as MovieDetail;
+export default async function MovieDetailPage({
+  params: {id},
+}: {
+  params: {id: MovieDetailType["id"]};
+}) {
+  const movie = (await getMovieById(id)) as MovieDetailType;
 
-  return (
-    <div>
-      <MovieDetailHero movieDetail={movie} />
-      <section className="grid gap-4 md:grid-cols-[1fr,300px]">
-        <div>
-          <Suspense fallback={<MoviesMediaSkeleton />}>
-            <MovieCharacters id={id} />
-          </Suspense>
-
-          <Suspense fallback={<MoviesMediaSkeleton />}>
-            <MovieImages id={id} />
-          </Suspense>
-
-          <Suspense fallback={<MovieGridSkeleton />}>
-            <MovieRecomendations id={id} />
-          </Suspense>
-        </div>
-        <MovieDetailSidebar info={movie} />
-      </section>
-    </div>
-  );
+  return <MovieDetail movie={movie} />;
 }
