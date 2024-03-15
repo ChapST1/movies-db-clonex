a9b4970f10d5e3d76954e491509a0f9016a7622e
ChapST1 <cutitox59@gmail.com>
2024-03-15 19:29:58 -0400

    add: Movie-Detail component

---
+++ src/components/movie-details/index.tsx
@@ -0,0 +1,38 @@
+import type {MovieDetail} from "@/types";
+
+import {Suspense} from "react";
+
+import {MoviesMediaSkeleton} from "../skeletons/movies-media-skeleton";
+import {MovieGridSkeleton} from "../skeletons/movie-grid-skeleton";
+
+import {MovieDetailHero} from "./movie-hero";
+import {MovieCharacters} from "./movie-characters";
+import {MovieImages} from "./movie-images";
+import {MovieRecomendations} from "./movie-recomendations";
+import {MovieDetailSidebar} from "./movie-sidebar";
+
+export function MovieDetail({movie}: {movie: MovieDetail}) {
+  const {id} = movie;
+
+  return (
+    <div>
+      <MovieDetailHero movieDetail={movie} />
+      <section className="grid gap-4 md:grid-cols-[1fr,300px]">
+        <div>
+          <Suspense fallback={<MoviesMediaSkeleton />}>
+            <MovieCharacters id={id} />
+          </Suspense>
+
+          <Suspense fallback={<MoviesMediaSkeleton />}>
+            <MovieImages id={id} />
+          </Suspense>
+
+          <Suspense fallback={<MovieGridSkeleton />}>
+            <MovieRecomendations id={id} />
+          </Suspense>
+        </div>
+        <MovieDetailSidebar info={movie} />
+      </section>
+    </div>
+  );
+}
