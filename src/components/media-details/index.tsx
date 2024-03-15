2b0f8da6e882ddc001d069e02fcb4dbbc863e93a
ChapST1 <cutitox59@gmail.com>
2024-03-15 19:27:09 -0400

    add: export components

---
+++ src/components/media-details/index.tsx
@@ -0,0 +1,47 @@
+// Backdrop image
+import {MediaDetailsBackdropPicture} from "./media-details-backdrops/media-details-backdrop-picture";
+import {MediaDetailsBackdropPictureImage} from "./media-details-backdrops/media-details-backdrop-picture-image";
+// Character
+import {MediaDetailsCharacterContainer} from "./media-details-character/media-details-character-container";
+import {MediaDetailsCharacterFooter} from "./media-details-character/media-details-character-footer";
+import {MediaDetailsCharacterFooterName} from "./media-details-character/media-details-character-footer-name";
+import {MediaDetailsCharacterPicture} from "./media-details-character/media-details-character-picture";
+import {MediaDetailsCharacterPictureImage} from "./media-details-character/media-details-character-picture-image";
+// Hero
+import {MediaDetailsHero} from "./media-details-hero/media-details-hero";
+import {MediaDetailsHeroContent} from "./media-details-hero/media-details-hero-content";
+import {MediaDetailsHeroContentFooter} from "./media-details-hero/media-details-hero-content-footer";
+import {MediaDetailsHeroContentGendersContainer} from "./media-details-hero/media-details-hero-content-genders-container";
+import {MediaDetailsHeroContentHeader} from "./media-details-hero/media-details-hero-content-header";
+import {MediaDetailsHeroContentHeaderTitle} from "./media-details-hero/media-details-hero-content-header-title";
+import {MediaDetailsHeroContentOverview} from "./media-details-hero/media-details-hero-content-overview";
+import {MediaDetailsHeroContentRuntime} from "./media-details-hero/media-details-hero-content-runtime";
+import {MediaDetailsHeroPicture} from "./media-details-hero/media-details-hero-picture";
+//Poster image
+import {MediaDetailsPosterPicture} from "./media-details-posters/media-details-poster-picture";
+import {MediaDetailsPosterPictureImage} from "./media-details-posters/media-details-poster-picture-image";
+
+export {
+  // Hero
+  MediaDetailsHero,
+  MediaDetailsHeroContent,
+  MediaDetailsHeroContentFooter,
+  MediaDetailsHeroContentGendersContainer,
+  MediaDetailsHeroContentHeader,
+  MediaDetailsHeroContentHeaderTitle,
+  MediaDetailsHeroContentOverview,
+  MediaDetailsHeroContentRuntime,
+  MediaDetailsHeroPicture,
+  // characters
+  MediaDetailsCharacterContainer,
+  MediaDetailsCharacterFooter,
+  MediaDetailsCharacterFooterName,
+  MediaDetailsCharacterPicture,
+  MediaDetailsCharacterPictureImage,
+  //backdrops
+  MediaDetailsBackdropPicture,
+  MediaDetailsBackdropPictureImage,
+  // poster
+  MediaDetailsPosterPicture,
+  MediaDetailsPosterPictureImage,
+};
