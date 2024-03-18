d7fed5b647012234efdcb000091c34f7bd56dc97
ChapST1 <cutitox59@gmail.com>
2024-03-18 17:52:49 -0400

    refactor: Remove unnecessary imports

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,16 +1,9 @@
-import Link from "next/link";
-import {FolderIcon} from "lucide-react";
-
-import {siteConfig} from "@/lib/constants/site-config";
-
-import {Separator} from "../ui/separator";
-
 import {SidebarSearchMovie} from "./sidebar-search";
 import {SidebarLinks} from "./sidebar-links";
 
 export function Sidebar() {
   return (
-    <nav className="fixed  left-0 top-16 z-30 flex h-[calc(100dvh-64px)] w-[200px] flex-col gap-6 overflow-y-scroll bg-background px-2 py-2">
+    <nav className="fixed  left-0 top-16 z-30 flex h-[calc(100dvh-64px)] w-[250px] flex-col gap-6 overflow-y-scroll bg-background px-2 py-2">
       <SidebarSearchMovie />
       <SidebarLinks />
     </nav>
