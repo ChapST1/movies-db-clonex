52552dfa66a311c9a383c51825454b6d013f30b4
ChapST1 <cutitox59@gmail.com>
2024-03-18 18:30:41 -0400

    style: Upgrade sidebar component

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,9 +1,17 @@
+import {twMerge} from "tailwind-merge";
+
 import {SidebarSearchMovie} from "./sidebar-search";
 import {SidebarLinks} from "./sidebar-links";
 
-export function Sidebar() {
+export function Sidebar({className, ...props}: React.ComponentProps<"nav">) {
   return (
-    <nav className="fixed  left-0 top-16 z-30 flex h-[calc(100dvh-64px)] w-[250px] flex-col gap-6 overflow-y-scroll bg-background px-2 py-2">
+    <nav
+      className={twMerge(
+        "fixed  left-0 top-16 z-30 flex h-[calc(100dvh-64px)] flex-col gap-6 overflow-y-scroll bg-background px-2 py-2",
+        className,
+      )}
+      {...props}
+    >
       <SidebarSearchMovie />
       <SidebarLinks />
     </nav>
