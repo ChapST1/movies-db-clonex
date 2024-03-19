33b6fad33edee34e9968acbecda502b6a8424cd5
ChapST1 <cutitox59@gmail.com>
2024-03-19 01:50:44 -0400

    refactor: Movie Item Component

--- src/components/movie/movie-item.tsx
+++ src/components/movie/movie-item.tsx
@@ -1,6 +1,7 @@
 import type {Movie} from "@/types";
 
 import Link from "next/link";
+import {twMerge} from "tailwind-merge";
 
 import {generateFullPath} from "@/lib/create-full-path";
 
@@ -10,18 +11,18 @@ import {MediaPictureImage} from "../media/media-picture-image";
 import {MediaFooter} from "../media/media-footer";
 import {MediaFooterName} from "../media/media-footer-name";
 
-export interface IsFilterProps {
-  enable: boolean;
+interface MediaProps extends React.ComponentProps<"picture"> {
+  movie: Movie;
 }
 
-export function MovieItem({movie}: {movie: Movie}) {
+export function MovieItem({movie, className, ...props}: MediaProps) {
   const {id, poster_path, title, backdrop_path, overview, release_date} = movie;
 
   const poster = generateFullPath({poster: {path: poster_path, size: "w185"}});
   const backdrop = generateFullPath({backdrop: {path: backdrop_path, size: "w300"}});
 
   return (
-    <MediaPicture className="group">
+    <MediaPicture className={twMerge("group", className)} {...props}>
       <Link className="" href={`/movies/${id}`}>
         <MediaPictureImage alt={title} src={poster} />
       </Link>
