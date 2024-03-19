522065a4e30ba80712a4be209dfe626d335b6fe9
ChapST1 <cutitox59@gmail.com>
2024-03-19 01:50:33 -0400

    refactor: Tv Item Component

--- src/components/tv/tv-item.tsx
+++ src/components/tv/tv-item.tsx
@@ -1,58 +1,45 @@
 import type {Tv} from "@/types";
-import type {IsFilterProps} from "../movie/movie-item";
 
 import Link from "next/link";
-import clsx from "clsx";
+import {twMerge} from "tailwind-merge";
 
 import {generateFullPath} from "@/lib/create-full-path";
 
-import {MediaBookmark} from "../media-bookmark";
 import {MediaOptions} from "../media-options";
+import {MediaPicture} from "../media/media-picture";
+import {MediaPictureImage} from "../media/media-picture-image";
+import {MediaFooter} from "../media/media-footer";
+import {MediaFooterName} from "../media/media-footer-name";
 
-export function TvItem({tv, isFilter}: {tv: Tv; isFilter?: IsFilterProps}) {
-  const {backdrop_path, poster_path, name, id, overview} = tv;
+interface MediaProps extends React.ComponentProps<"picture"> {
+  tv: Tv;
+}
+
+export function TvItem({tv, className, ...props}: MediaProps) {
+  const {backdrop_path, poster_path, name, id, overview, first_air_date} = tv;
   const poster = generateFullPath({poster: {path: poster_path, size: "w185"}});
   const backdrop = generateFullPath({backdrop: {path: backdrop_path, size: "w300"}});
 
-  const isFilterMedia = clsx({
-    hidden: isFilter && !isFilter.enable,
-    block: isFilter && isFilter.enable,
-  });
-
   return (
-    <article
-      className={`group relative z-0 min-h-[200px] min-w-[150px] overflow-hidden ${isFilterMedia}`}
-    >
-      <Link className="group" href={`/tv/${id}`}>
-        <picture className="rounded-media relative block flex-grow overflow-hidden">
-          <img
-            alt={name}
-            className="aspect-[0.7] h-full w-full  object-cover duration-300 group-hover:scale-105"
-            loading="lazy"
-            src={poster}
-          />
-        </picture>
-
-        <footer className="w-full rounded-sm bg-background p-2">
-          <h3 className="line-clamp-1" title={name}>
-            {name}
-          </h3>
-        </footer>
+    <MediaPicture className={twMerge("group", className)} {...props}>
+      <Link href={`/tv/${id}`}>
+        <MediaPictureImage alt={name} src={poster} />
       </Link>
-      <MediaBookmark
-        className="absolute right-1 top-1 size-9 opacity-0 group-hover:opacity-100 md:size-10"
-        mediaId={tv.id}
-      />
+
+      <MediaFooter>
+        <MediaFooterName>{name}</MediaFooterName>
+      </MediaFooter>
+
       <MediaOptions
         overviewInfo={{
           backdrop: `${backdrop}`,
           poster: `${poster}`,
-          date: "",
+          date: first_air_date,
           id,
           info: overview,
           title: name,
         }}
       />
-    </article>
+    </MediaPicture>
   );
 }
