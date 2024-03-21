a6e53ec2e3b931b686f8d6ce032a978309bb8d76
ChapST1 <cutitox59@gmail.com>
2024-03-21 02:17:06 -0400

    style: add a maskImage

--- src/components/media-details/media-details-hero/media-details-hero-picture.tsx
+++ src/components/media-details/media-details-hero/media-details-hero-picture.tsx
@@ -13,7 +13,14 @@ export function MediaDetailsHeroPicture({
 }: MediaDetailsHeroPicture) {
   return (
     <picture className={twMerge("relative flex items-center justify-center", className)} {...props}>
-      <img alt={title} className="aspect-[1/0] w-44 pb-5 md:w-auto md:pb-0" src={posterPath} />
+      <img
+        alt={title}
+        className="aspect-[1/0] w-44 pb-5 md:w-auto md:pb-0"
+        src={posterPath}
+        style={{
+          maskImage: "linear-gradient(to bottom, black 80%, transparent 100%)",
+        }}
+      />
     </picture>
   );
 }
