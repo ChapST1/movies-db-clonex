3f565e22be0f9ea0886a32487c0eeadc1e5491d8
ChapST1 <cutitox59@gmail.com>
2024-03-22 02:29:08 -0400

    refactor: SidebarLink Component

--- src/components/sidebar/sidebar-links/index.tsx
+++ src/components/sidebar/sidebar-links/index.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import {FolderIcon} from "lucide-react";
 import clsx from "clsx";
 import {usePathname} from "next/navigation";
 import Link from "next/link";
@@ -10,37 +9,51 @@ import {Separator} from "@/components/ui/separator";
 
 export function SidebarLinks() {
   const pathname = usePathname();
+  const {
+    links: {primaryLinks, secondaryLinks},
+  } = siteConfig;
 
   return (
-    <>
-      <ul className="flex flex-col gap-2">
-        {siteConfig.links.map(({name, path, icon}) => {
-          const activeStyles = clsx({
-            "border-gradient bg-secondary pl-3 text-gradient": path === pathname,
-            "border-border bg-background": path !== pathname,
-          });
+    <ul className="flex flex-col gap-2">
+      {primaryLinks.map(({name, path, icon}) => {
+        const activeStyles = clsx({
+          "border-gradient bg-secondary pl-3 text-gradient": path === pathname,
+          "border-border bg-background": path !== pathname,
+        });
 
-          return (
-            <li key={name}>
-              <Link
-                className={`flex w-full items-center justify-start gap-2 rounded-md border-2  p-2 duration-300 hover:border-gradient hover:bg-secondary hover:pl-3 hover:text-gradient [&>*:nth-child(1)]:size-4 ${activeStyles}`}
-                href={path}
-              >
-                {icon}
-                {name}
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
-      <Separator />
-      <Link
-        className="flex items-center justify-start gap-2 rounded-md border-2 border-border bg-background p-2 duration-300 hover:border-gradient hover:bg-secondary hover:pl-3 hover:text-gradient [&>*:nth-child(1)]:size-4"
-        href="path"
-      >
-        <FolderIcon />
-        Categories
-      </Link>{" "}
-    </>
+        return (
+          <li key={name}>
+            <Link
+              className={`flex w-full items-center justify-start gap-2 rounded-md border-2  p-2 duration-300 hover:border-gradient hover:bg-secondary hover:pl-3 hover:text-gradient [&>*:nth-child(1)]:size-4 ${activeStyles}`}
+              href={path}
+            >
+              {icon}
+              {name}
+            </Link>
+          </li>
+        );
+      })}
+
+      <Separator className="my-5" />
+
+      {secondaryLinks.map(({name, path, icon}) => {
+        const activeStyles = clsx({
+          "border-gradient bg-secondary pl-3 text-gradient": path === pathname,
+          "border-border bg-background": path !== pathname,
+        });
+
+        return (
+          <li key={name}>
+            <Link
+              className={`flex w-full items-center justify-start gap-2 rounded-md border-2  p-2 duration-300 hover:border-gradient hover:bg-secondary hover:pl-3 hover:text-gradient [&>*:nth-child(1)]:size-4 ${activeStyles}`}
+              href={path}
+            >
+              {icon}
+              {name}
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
   );
 }
--- src/lib/constants/site-config.tsx
+++ src/lib/constants/site-config.tsx
@@ -1,14 +1,25 @@
-import {Clapperboard, Home, Tv, UserRound} from "lucide-react";
+import {Clapperboard, FolderIcon, Home, SearchIcon, Tv, UserRound} from "lucide-react";
 
 /**
  * Siteconfig - Routes
  */
-const links = [
+
+const primaryLinks = [
   {name: "Home", path: "/", icon: <Home />},
   {name: "Movies", path: "/movies", icon: <Clapperboard />},
   {name: "Tv Series", path: "/tv", icon: <Tv />},
   {name: "People", path: "/people", icon: <UserRound />},
 ];
+
+const secondaryLinks = [
+  {name: "Categories", path: "/categories", icon: <FolderIcon />},
+  {name: "Search", path: "/search", icon: <SearchIcon />},
+];
+
+const links = {
+  primaryLinks,
+  secondaryLinks,
+};
 
 export const siteConfig = {
   links,
