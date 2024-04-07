47169b11bc0124a1f562c02a066be7409b3c2c82
ChapST1 <cutitox59@gmail.com>
2024-04-06 22:51:51 -0400

    style: Upgrade ShadowEffect Component - responsive

--- src/components/shadow-effect.tsx
+++ src/components/shadow-effect.tsx
@@ -3,7 +3,7 @@ import {twMerge} from "tailwind-merge";
 export function ShadowEffect({className, ...props}: React.ComponentProps<"footer">) {
   return (
     <footer
-      className={twMerge("sticky bottom-[-1px] left-0 z-30 h-[100px] w-full", className)}
+      className={twMerge("sticky bottom-[-1px] left-0 z-30 h-16 w-full md:h-[100px]", className)}
       {...props}
       style={{
         backgroundImage:
