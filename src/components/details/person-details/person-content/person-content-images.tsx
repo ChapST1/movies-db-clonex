319b80a33b34425c330c86d2ae24d31ce84f5d04
ChapST1 <cutitox59@gmail.com>
2024-04-10 19:20:03 -0400

    add: grid style - hanlder

--- src/components/details/person-details/person-content/person-content-images.tsx
+++ src/components/details/person-details/person-content/person-content-images.tsx
@@ -1,5 +1,7 @@
 import type {Person, PersonProfiles} from "@/types";
 
+import clsx from "clsx";
+
 import {Title} from "@/components/ui/title";
 import {generateFullPath} from "@/lib/create-full-path";
 import {MediaContainer} from "@/components/ui/media-container";
@@ -9,12 +11,17 @@ import {MediaGrid} from "@/components/media-grid";
 export async function PersonContentImages({id, owner}: {id: Person["id"]; owner: Person["name"]}) {
   const {profiles} = (await getPersonGallery({id})) as PersonProfiles;
 
+  const gridStyles = clsx({
+    "md:grid-cols-4 grid-cols-3": profiles.length < 5,
+    "grid-cols-4": profiles.length > 5,
+  });
+
   return (
     <div>
       <Title>Profiles</Title>
 
       <MediaContainer mediaLength={profiles.length}>
-        <MediaGrid>
+        <MediaGrid className={gridStyles}>
           {profiles.map(({file_path}, index) => {
             const path = generateFullPath({profile: {path: file_path, size: "w185"}});
 
