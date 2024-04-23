e2024ac1773b4c8889e7eba1cf9406586540dcb2
ChapST1 <cutitox59@gmail.com>
2024-04-22 21:55:23 -0400

    refactor: use Sheet Components

--- src/components/ui/modal/modal-content.tsx
+++ src/components/ui/modal/modal-content.tsx
@@ -1,13 +1,19 @@
 import {twMerge} from "tailwind-merge";
 
-type CurrentProps = React.ComponentProps<"article">;
+import {SheetContent} from "@/components/ui/sheet";
+
+type CurrentProps = React.ComponentProps<"div">;
 
 export function ModalContent({children, ...props}: CurrentProps) {
   return (
-    <article
-      className={twMerge("my-2 w-[90%] border border-border bg-background", props.className)}
+    <SheetContent
+      className={twMerge(
+        "h-[90dvh] max-h-[90dvh] min-h-[90dvh] overflow-y-auto p-0",
+        props.className,
+      )}
+      side="bottom"
     >
-      {children}
-    </article>
+      <div>{children}</div>
+    </SheetContent>
   );
 }
