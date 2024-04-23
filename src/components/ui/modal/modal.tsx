bbd75e0006c27c8f8b304a9890566490241a8996
ChapST1 <cutitox59@gmail.com>
2024-04-22 21:56:23 -0400

    refactor: Use Sheet Component

--- src/components/ui/modal/modal.tsx
+++ src/components/ui/modal/modal.tsx
@@ -1,42 +1,28 @@
 "use client";
 
 import {useRouter} from "next/navigation";
-import {useEffect, useRef} from "react";
 import {twMerge} from "tailwind-merge";
+
+import {Sheet} from "@/components/ui/sheet";
 
 type CurrentProps = React.ComponentProps<"div">;
 
-// use dialog tag
 export function Modal({children, ...props}: CurrentProps) {
-  const modalRef = useRef<HTMLDivElement | null>(null);
   const router = useRouter();
 
-  const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
-    if (!modalRef.current) return;
-
-    if (event.target === modalRef.current) {
-      router.back();
-    }
-  };
-
-  useEffect(() => {
-    document.body.style.overflowY = "hidden";
-
-    return () => {
-      document.body.style.removeProperty("overflow-y");
-    };
-  }, []);
-
   return (
-    <div
-      ref={modalRef}
-      className={twMerge(
-        "fixed left-0 top-16 z-[50] flex h-[calc(100dvh-64px)] w-full items-center justify-center overflow-y-auto bg-background/85 *:border *:border-border md:backdrop-blur-sm",
-        props.className,
-      )}
-      onClick={handleClick}
+    <Sheet
+      modal
+      open
+      onOpenChange={(e) => {
+        if (!e) {
+          router.back();
+        }
+      }}
     >
-      {children}
-    </div>
+      <div className={twMerge("fixed overflow-y-auto", props.className)} {...props}>
+        {children}
+      </div>
+    </Sheet>
   );
 }
